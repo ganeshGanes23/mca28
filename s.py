b42b95d1682246f85767b84e207df587ca9d7886
ganesh <ganeshganesh39928@gmail.com>
2023-12-23 04:02:07 -0800

    third commit

---
+++ s.py
@@ -0,0 +1,35 @@
+'''var=10 
+for i in range(10):
+    for j in range(2,10,1):
+        if var%2==0:
+            continue
+            var+=1
+        var+=1
+    else:
+        var+=1
+    print(var)'''
+''' x=['XX','YY']   
+    for i in x:
+        i.lower()
+    print(x)'''
+'''def power(x,y=2):
+    r=1
+    for i in range(y):
+        r=r*X
+    return r
+    print(power(3))
+    print(power(3,3))
+power(3,)'''
+
+'''for num in range(10,14):
+    for i in range(2,num):
+        if num%i==0:
+            break
+        else:
+            print(num)'''
+
+def C2F(c):
+    return c*9/5+32
+print(C2F(100))
+print(C2F(0))
+
